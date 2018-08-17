5aab49c96616243cdd053150c57e271ae322e50f
Zverloff <toddzverloff@gmail.com>
2018-08-17 13:40:52 -0400

    getting there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,89 @@
+//shortcut variables
+var newGameButton = document.getElementById("start");
+var letterSpaces = document.getElementById("spaces");
+var guessedLetters = document.getElementById("guessedDiv");
+var guessesRemaining = document.getElementById("score");
+var victory = document.getElementById("wins");
+var failure = document.getElementById("losses");
+
+//creating word bank
 var wordChoice = ["sword", "spear", "shield", "pillage", "longship", "dragon", "serpent"];
 
-// select word to guess
-var getWord = wordChoice[Math.floor(Math.random() * wordChoice.length)];
+//defining variables
+var win = 0;
+var loss = 0;
+var guessesLeft = 9;
+var chosenWordArray = [];
+var letterArray = [];
+var guessedArray = [];
+var word = " ";
+var gameStart = false
 
-// answer array
-var answers = [];
-for (var i = 0; i < getWord.length; i++) {
-    answers[i] = "_";
+//button to start new game
+newGameButton.addEventListener("click", newGame);
+
+//capturing keystrokes
+document.onkeyup = function(event) {    
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        guessLetter(event.key);
+    }
 }
-var remaining = getword.length; 
 
-while (remaining > 0) {
-    document.write(answers.join(" "))
+//setting the new game conditions
+function newGame() {
+    guessesLeft = 9;
+    chosenWordArray = [];
+    letterArray = [];
+    guessedArray = [];
+    gameStart= true
 
+//getting a random word
+word = wordChoice[Math.floor(Math.random() * wordChoice.length)];
+
+//changing the random word to spaces
+for (var i = 0; i < word.length; i++) {
+    if (word[i] === " ") {
+        chosenWordArray.push("_");
+    }
+    else  {
+        chosenWordArray.push("_");
+    }
+}
+
+//putting elements on the DOM
+guessesRemaining.textContent = guessesLeft;
+letterSpaces.textContent = chosenWordArray.join(" ");
+guessedArray.textContent = guessedDiv;
+}
+
+//function to define logic of checking if the letter is in the word
+function guessLetter(letter) { 
+    //checking to see if buttn has been clicked to start and that a letter has only been pressed once
+    if (gameStart === true && guessedArray.indexOf(letter) === -1) {
+        //pushing used letter to an array
+        guessedArray.push(letter);
+
+        //turning letters to lower case and checking the word against the word in the array
+        for (var i = 0; i < word.length; i++) {
+            if (word[i].toLowerCase() === letter.toLowerCase()) {
+                chosenWordArray[i] = word[i];
+            }    
+        }
+
+        //writing new word to the DOM with the letters filled in
+        letterSpaces.textContent = choseWordArray.join(" ");
+        
+    }
+    //if game isn't started or a letter is pressed more than once, alerting the player of what to
+    else { 
+        if (gameStart === false) {
+         alert("Please start a new game");
+        }
+        else {
+            alert("You have already tried that letter")
+        }
+    }
 }
 
 
-
+ 
